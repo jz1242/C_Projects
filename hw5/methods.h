4e8879cf693f03a33bbae7e9ff9921f7baa0057b
Jason Zhang <jz1242@users.noreply.github.com>
2017-04-02 17:50:20 -0400

    Add files via upload

--- hw5/methods.h
+++ hw5/methods.h
@@ -1,8 +1,7 @@
-/*
- * methods.h
- *
- *  Created on: Mar 28, 2017
- *      Author: JZ
+/* File: hw5.c
+ Homework 5, 600.120 Spring 2017 3/8
+
+ Jason Zhang jzhan127 jzhan127@jhu.edu
  */
 
 #ifndef METHODS_H_
@@ -25,6 +24,29 @@ typedef struct nodeStruct{
     struct nodeStruct *next;
 } node;
 
+Course *data;
+Course createCourse(char *str);
+node* Create(Course course, char* year, char* grade);
+void InsertAfter(node *e, Course course, char* year, char* grade);
+void InsertHead(node **head, Course course, char* year, char* grade);
+void DeleteAfter(node *e);
+void DeleteHead(node **head);
+int checkValidSem(char *a);
+void printCourses(int num);
+int checkCase(char* a, char* b);
+int compareCourse(Course a, Course b);
+int searchCourse(int num, char *str);
+int searchTranscript(node **head, Course course);
+int searchTranscriptNoDel(node **head, Course course);
+void printTranscript(node **head, int count);
+void printCourseData(node **head, Course course);
+int checkValidCourse(char *a);
+int checkValidTitle(char *a);
+int checkValidCred(char *a);
+int checkValidGrade(char *a);
+int checkDup(node **head, Course course, char *sem);
+double gradeConvert(char *a);
+void gpaCalc(node **head, int count);
 
 
 #endif /* METHODS_H_ */
---
+++ hw5/unittest.c
@@ -0,0 +1,131 @@
+/* File: unittest.c
+ Homework 5, 600.120 Spring 2017 4/2
+
+ Jason Zhang jzhan127 jzhan127@jhu.edu
+ */
+#include <stdio.h>
+#include <assert.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <stdbool.h>
+#include "methods.h"
+#include "prompts.h"
+
+void testCreateCourse();
+void testCheckCase();
+void testCompareCourse();
+void testSearchCourse();
+void testSearchTranscript();
+void testSearchTranscriptNoDel();
+void testCheckValidCourse();
+void testCheckValidTitle();
+void testCheckValidCred();
+void testCheckValidSem();
+void testCheckValidGrade();
+void testCheckDup();
+void testGradeConvert();
+
+int main() {
+    testCreateCourse();
+    testCheckCase();
+    testCompareCourse();
+    testSearchCourse();
+    testSearchTranscript();
+    testSearchTranscriptNoDel();
+    testCheckValidCourse();
+    testCheckValidTitle();
+    testCheckValidCred();
+    testCheckValidSem();
+    testCheckValidGrade();
+    testCheckDup();
+    testGradeConvert();
+    printf("All tests passed");
+    return 0;
+}
+
+void testCreateCourse() {             //takes in input from dna1 to test the function
+    char str[100] = "EN.550.171 4.0 Discrete Mathematics";
+    Course course = createCourse(str);
+    assert(strcmp(course.div, "EN") == 0);
+    assert(course.dep == 550);//test int arrays of the strings inputted
+    assert(course.num == 171);
+    assert(strcmp(course.title, "Discrete Mathematics") == 0);
+}
+
+void testCheckCase(){
+    char a[3] = "EN";
+    char b[3] = "en";
+    int c = checkCase(a, b);
+    assert(c == 1);
+}
+
+void testCompareCourse(){
+    char str[] = "en.550.171 4.0 Discrete Mathematics";
+    Course course1 = createCourse(str);
+    Course course2 = createCourse(str);
+    int a = compareCourse(course1, course2);
+    assert(a == 1);
+
+}
+
+void testSearchCourse(){
+    FILE *input = fopen("catalog1.txt", "r");      //get file and take user inpt
+    int index = 0;  //variable counters
+    int size = 10;
+    char temp[100];
+    Course temp2;
+    data = malloc(size * sizeof(Course));
+    while (fgets(temp, 100, input)) {
+        if (index >= size) {
+            size *= 2;
+            data = realloc(data, size * sizeof(Course));
+        }
+        temp2 = createCourse(temp);
+        data[index] = temp2;
+        index++;
+    }
+    char inp[] = "en.550.171";
+    fclose(input);
+    assert(searchCourse(index, inp) == 0);
+}
+
+void testSearchTranscript(){
+    node *head = Create(data[0], "1000.s", "A+");
+    assert(searchTranscript(&head, data[0]) == 1);
+}
+void testSearchTranscriptNoDel(){
+    node *head = Create(data[0], "1000.s", "A+");
+    assert(searchTranscriptNoDel(&head, data[0]) == 1);
+}
+
+void testCheckValidCourse(){
+    char inp[]="en.550.171";
+    assert(checkValidCourse(inp) == 1);
+}
+
+void testCheckValidTitle(){
+    char inp[]="DiscreteMathematics";
+    assert(checkValidTitle(inp) == 1);
+}
+void testCheckValidCred(){
+    char inp[] = "4.0";
+    assert(checkValidCred(inp) == 1);
+}
+void testCheckValidSem(){
+    char inp[] = "1999.s";
+    assert(checkValidSem(inp) == 1);
+}
+
+void testCheckValidGrade(){
+    assert(checkValidGrade("A+") == 1);
+}
+void testCheckDup(){
+    node *head = Create(data[0], "1000.s", "A+");
+    assert(checkDup(&head, data[0], "1000.s") == 1);
+}
+
+void testGradeConvert(){
+    assert(gradeConvert("A+") == 4.0);
+}
+
